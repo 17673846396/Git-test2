40ae2c4d8119e05029a05edaeabb8ef59b11f0ed
taoy <taoy@pengding.loc>
2021-11-07 09:48:52 +0800

    hot-fix11

--- src/main/java/taoy/pending/git/Gittest2.java
+++ src/main/java/taoy/pending/git/Gittest2.java
@@ -6,5 +6,6 @@ public class Gittest2 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git4");
+        System.out.println("hello hot-fix11");
     }
 }
